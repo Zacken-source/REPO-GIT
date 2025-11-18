092621184b514efcc224c1d3ae95243ba6f5bbb8
zacken <mchereau05@gmail.com>
2025-11-18 22:12:26 +0100

    change color with button

---
+++ Exo_2/scripte.js
@@ -0,0 +1,12 @@
+function clicButton(){
+    alert("Bouton cliqué !")
+}
+
+function clicgreenButton() {
+    let titleBecomeGreen = document.getElementById("titleButton");
+    titleBecomeGreen.style.color = "green";
+}
+function clicblueButton() {
+    let titleBecomeBlue = document.getElementById("titleButton");
+    titleBecomeBlue.style.color = "blue";
+}
