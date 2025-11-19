064f202fb068b6d4f2a1c3b86dcbe5e3ac72a6ed
zacken <mchereau05@gmail.com>
2025-11-19 15:51:07 +0100

    test with switch

---
+++ Exo_6/scripte.js
@@ -0,0 +1,12 @@
+let fruit = "mangue";
+
+switch (fruit) {
+    case "fraise":
+        console.log("c'est fraise");
+        break;
+    case "mangue":
+        console.log("c'est mangue");
+        break;
+    default:
+        console.log("et bah y'a pas de fruit sur la liste de ce que tu veux")
+}
