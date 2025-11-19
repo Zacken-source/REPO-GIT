be7900cadc686b5596d5daf9051e38d093621c7f
zacken <mchereau05@gmail.com>
2025-11-19 16:01:32 +0100

    text with if

---
+++ Exo_7/scripte.js
@@ -0,0 +1,7 @@
+let bibliotheque = ["le premier livre","le deuxieme livre","le troisieme livre", "le sandwiche n'a rien à faire dans une librairie"];
+let max = 4
+if (bibliotheque.length<=max){
+    console.log("Il y a surement des livres qui sont emprunter mais dans la logique tout est bon en attendant");
+}else{
+    console.log("Il y a un pb dans les comptes");
+}
